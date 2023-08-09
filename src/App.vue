297705819e2dd2d24f77933bedec8ee002aeeef4
zanniadevweb <zanni.a.dev.web@gmail.com>
2023-08-09 14:11:29 +0200

    feat: add v-show conditionnal rendering

--- src/App.vue
+++ src/App.vue
@@ -26,8 +26,9 @@
                   <h4>Type d'ouvrage : livre ou article ?</h4>
                   <v-select
                     id="inputBookOrArticle"
+                    v-model="inputBookOrArticle"
                     class="text-black font-weight-black"
-                    onchange="hideIsContributionCheckBox()"
+                    @update:modelValue="hideIsContributionCheckBox($event)"
                     :items="['Livre', 'Article']"
                   />
                   <span id="spanIsContributionCheckBox">
@@ -37,25 +38,35 @@
                           <h4>Il s'agit d'un document électronique ?</h4>
                         </v-responsive>
                       </v-col>
-                      <v-checkbox id="checkboxIsElectronicDocument" onchange="hideElectronicDocumentInputs()" />
+                      <v-checkbox
+                        id="checkboxIsElectronicDocument"
+                        v-model="checkboxIsElectronicDocument"
+                        @change="hideElectronicDocumentInputs()"
+                        color="#1e90ff"
+                      />
                     </v-row>
                   </span>
-                  <span id="spanIsContributionCheckBox">
+                  <span v-show="selectIsBook" id="spanIsContributionCheckBox">
                     <v-row justify="space-between">
                       <v-col cols="auto">
                         <v-responsive width="350">
                           <h4>L'ouvrage doit en citer un autre ?</h4>
                         </v-responsive>
                       </v-col>
-                      <v-checkbox id="checkboxInsidePublication" onchange="hideInsidePublicationInputs()" />
+                      <v-checkbox
+                        id="checkboxInsidePublication"
+                        v-model="checkboxInsidePublication"
+                        @change="hideInsidePublicationInputs()"
+                        color="#1e90ff"
+                      />
                     </v-row>
                   </span>
-                  <span id="spanWithShortCitationSelectMain">
+                  <span v-show="checkboxInsidePublication" id="spanWithShortCitationSelectMain">
                     <h4>Référence principale avec citation courte :</h4>
                     <v-select
-                      id="inputBookOrArticle"
+                      id="inputShortCitationMain"
+                      v-model="inputShortCitationMain"
                       class="text-black font-weight-black"
-                      onchange="hideIsContributionCheckBox()"
                       :items="['Oui', 'Non']"
                     />
                   </span>
@@ -84,18 +95,19 @@
                       <v-col cols="auto">
                         <v-responsive width="300">
                           <v-text-field
-                            id="inputPPN"
+                            id="isInputPPNFilled"
+                            @update:modelValue="disableBtnConfirmPpn($event)"
                             class="text-black font-weight-black"
-                            placeholder="170583236"
+                            placeholder="Ex. de valeur : 170583236"
                           />
                         </v-responsive>
                       </v-col>
                       <v-col cols="auto">
                         <v-btn
                           id="buttonConfirmPPN"
-                          onclick="getXmlByPPN()"
-                          color="grey"
-                          disabled
+                          @click="getXmlByPPN()"
+                          color="green"
+                          :disabled='isInputPPNUnfilled'
                         >
                           CONFIRMER PPN
                         </v-btn>
@@ -115,7 +127,7 @@
                   <v-container>
                     <v-btn
                       id="v-btnGenerateReference"
-                      onclick="buildReference()"
+                      @click="buildReference()"
                       variant="outlined"
                       color="white"
                       class="font-weight-bold"
@@ -124,26 +136,28 @@
                     </v-btn>
                   </v-container>
                   <v-divider class="my-12" />
-                  <v-container>
-                    <v-row justify="space-between">
-                      <h2>Résultat :</h2>
-                      <v-btn
-                        prepend-icon="mdi-content-copy"
-                        text="Copier Résultat"
-                        @click="copyTextFromInput"
-                      />
-                    </v-row>
+                  <v-card color="white">
                     <v-container>
-                      <h3>
-                        <input
-                          id="spanResult"
-                          style="font-family: 'Arial'; color:black; font-size: 1em; border: none; outline: none;"
-                          value="..."
-                          readonly
-                        >
-                      </h3>
+                      <v-row justify="space-between">
+                        <h2>Résultat :</h2>
+                        <v-btn
+                          prepend-icon="mdi-content-copy"
+                          text="Copier Résultat"
+                          @click="copyTextFromInput"
+                        />
+                      </v-row>
+                      <v-container>
+                        <h3>
+                          <input
+                            id="spanResult"
+                            style="font-family: 'Arial'; color:black; font-size: 1em; border: none; outline: none;"
+                            value="..."
+                            readonly
+                          >
+                        </h3>
+                      </v-container>
                     </v-container>
-                  </v-container>
+                  </v-card>
                 </v-container>
 
                 <v-divider class="my-12" />
@@ -154,10 +168,10 @@
                     <h4><label>Auteurs (Facultatif)</label></h4>
                     <v-combobox
                       id="inputAuthorsMain"
-                      v-model="select"
-                      placeholder="Dupont, S., M."
+                      v-model="selectAuthorsMain"
+                      placeholder="Ex. de valeur : Dupont, S., M."
                       class="text-black font-weight-black"
-                      :items="items"
+                      :items="itemsAuthorsMain"
                       label="Dupont, S., M."
                       multiple
                       chips
@@ -167,59 +181,60 @@
                     Éd. ou Dir. ou Coll. ou Auteur :
                     <v-select
                       id="inputEdOrDirOrCollMain"
+                      v-model="inputEdOrDirOrCollMain"
                       class="text-black font-weight-black"
-                      onchange="hideIsContributionCheckBox()"
+                      @update:modelValue="hideIsContributionCheckBox()"
                       :items="['éd.', 'dir.', 'coll.', 'Auteur']"
                     />
                     Date de première publication (Facultatif) :
                     <v-text-field
                       id="inputDateFirstMain"
                       class="text-black font-weight-black"
-                      placeholder="1995"
+                      placeholder="Ex. de valeur : 1995"
                     />
                     Date de publication (Facultatif) :
                     <v-text-field
                       id="inputDateMain"
                       class="text-black font-weight-black"
-                      placeholder="2003"
+                      placeholder="Ex. de valeur : 2003"
                     />
                     Pages (Facultatif) :
                     <v-text-field
                       id="inputPagesMain"
                       class="text-black font-weight-black"
-                      placeholder="467-489"
+                      placeholder="Ex. de valeur : 467-489"
                     />
                     <span id="spanWithShortCitationUselessItems">
                       Collection / Revue (Facultatif) :
                       <v-text-field
                         id="inputEditorMain"
                         class="text-black font-weight-black"
-                        placeholder="Gallia Suppl."
+                        placeholder="Ex. de valeur : Gallia Suppl."
                       />
                       N° Collection / Tomaison (Facultatif) :
                       <v-text-field
                         id="inputNumberEditorMain"
                         class="text-black font-weight-black"
-                        placeholder="59"
+                        placeholder="Ex. de valeur : 59"
                       />
                       Lieu de publication (Facultatif) :
                       <v-text-field
                         id="inputLocationMain"
                         class="text-black font-weight-black"
-                        placeholder="Paris"
+                        placeholder="Ex. de valeur : Paris"
                       />
                       <span id="spanWithBookInputs">
                         Informations complémentaires édition (Facultatif) :
                         <v-text-field
                           id="inputBonusInfoEditionMain"
                           class="text-black font-weight-black"
-                          placeholder="trad. fr. d’après la 2e éd. anglaise de 1957, 1re éd. Oxford"
+                          placeholder="Ex. de valeur : trad. fr. d’après la 2e éd. anglaise de 1957, 1re éd. Oxford"
                         />
                         N° Romain de Volume (Facultatif) :
                         <v-text-field
                           id="inputRomanNumberMain"
                           class="text-black font-weight-black"
-                          placeholder="v"
+                          placeholder="Ex. de valeur : v"
                         />
                         Genre (Facultatif) :
                         <v-text-field
@@ -231,34 +246,34 @@
                         <v-text-field
                           id="inputUniversityMain"
                           class="text-black font-weight-black"
-                          placeholder="Université Bordeaux Montaigne"
+                          placeholder="Ex. de valeur : Université Bordeaux Montaigne"
                         />
                       </span>
                     </span>
-                    <span id="spanWithElectronicDocumentInputs">
+                    <span v-show="checkboxIsElectronicDocument" id="spanWithElectronicDocumentInputs">
                       Date électronique de mise en ligne :
                       <v-text-field
                         id="inputDateElectronicUploadMain"
                         class="text-black font-weight-black"
-                        placeholder="06 février 2003"
+                        placeholder="Ex. de valeur : 06 février 2003"
                       />
                       Date électronique de consultation :
                       <v-text-field
                         id="inputDateElectronicConsultMain"
                         class="text-black font-weight-black"
-                        placeholder="30 décembre 2022"
+                        placeholder="Ex. de valeur : 30 décembre 2022"
                       />
                       Type ressource électronique :
                       <v-select
                         id="inputDoiOrUrlMain"
                         class="text-black font-weight-black"
-                        onchange="changeTextInputUrlMain()"
+                        @update:modelValue="changeTextInputUrlMain()"
                         :items="['DOI', 'URL']"
                       />
                       <v-text-field
                         id="inputUrlMain"
                         class="text-black font-weight-black"
-                        placeholder="10.1016/S8756-3282(01)00704-9"
+                        placeholder="Ex. de valeur : 10.1016/S8756-3282(01)00704-9"
                       />
                     </span>
                   </v-container>
@@ -266,47 +281,51 @@
 
                 <v-divider class="my-12" />
 
-                <v-container id="secondarySection">
+                <v-container v-show="checkboxInsidePublication" id="secondarySection">
                   <span
                     id="spanWithInsidePublicationInputs"
                   >
                     <span id="spanSaisir"><h3>Référence contenue dans la principale :</h3></span>
                     <v-container>
                       <h4><label>Auteurs (Facultatif)</label></h4>
-                      <v-text-field
+                      <v-combobox
                         id="inputAuthorsInside"
+                        v-model="selectAuthorsInside"
+                        placeholder="Ex. de valeur : Dupont, S., M."
                         class="text-black font-weight-black"
-                        list="speakers"
-                        placeholder="Dupont, S., M."
-                        style="font-size: 16.5pt;"
-                      />
+                        :items="itemsAuthorsInside"
+                        label="Dupont, S., M."
+                        multiple
+                        chips
+                      ></v-combobox>
                     </v-container>
 
                     <v-container>
                       Éd. ou Dir. ou Coll. ou Auteur :
                       <v-select
                         id="inputEdOrDirOrCollInside"
+                        v-model="inputEdOrDirOrCollInside"
                         class="text-black font-weight-black"
-                        onchange="hideIsContributionCheckBox()"
+                        @update:modelValue="hideIsContributionCheckBox()"
                         :items="['éd.', 'dir.', 'coll.', 'Auteur']"
                       />
                       Date de publication (Facultatif) :
                       <v-text-field
                         id="inputDateInside"
                         class="text-black font-weight-black"
-                        placeholder="2003"
+                        placeholder="Ex. de valeur : 2003"
                       />
                       Date de première publication (Facultatif) :
                       <v-text-field
                         id="inputDateFirstInside"
                         class="text-black font-weight-black"
-                        placeholder="2003"
+                        placeholder="Ex. de valeur : 2003"
                       />
                       Titre d'ouvrage (Pour obtenir : saisir sup sup)
                       <v-text-field
                         id="inputTitleInside"
                         class="text-black font-weight-black"
-                        placeholder="Plaidoyer pour la ‘petite épigraphie’&nbsp;: l’exemple de la cité de Béziers"
+                        placeholder="Ex. de valeur : Plaidoyer pour la ‘petite épigraphie’&nbsp;: l’exemple de la cité de Béziers"
                       />
                     </v-container>
                   </span>
@@ -331,17 +350,17 @@
             SUDOC via un n°PPN d'ouvrage pour pré-remplir les champs. Aucune case
             ne peut être cochée en ce but. Seuls les livres sont concernés par cette fonctionnalité.
           <v-container>
-            <li>Développeur :
-              <a
-                style="color: dodgerblue; text-decoration: none; font-weight: bold"
-                href="https://ausonius.u-bordeaux-montaigne.fr/annuaire?chercheur=346"
-                target="_blank"
-              >
-                Alexandre Zanni (Doctorant Archéologie, UMR 5607 Ausonius)
-              </a>
-            </li>
-            <li>Consultant et testeur : Julien Gravier (Doctorant Archéologie, UMR 5607 Ausonius)</li>
-          </v-container>
+          <li>Développeur :
+            <a
+              style="color: dodgerblue; text-decoration: none; font-weight: bold"
+              href="https://ausonius.u-bordeaux-montaigne.fr/annuaire?chercheur=346"
+              target="_blank"
+            >
+              Alexandre Zanni (Doctorant Archéologie, UMR 5607 Ausonius)
+            </a>
+          </li>
+          <li>Consultant et testeur : Julien Gravier (Doctorant Archéologie, UMR 5607 Ausonius)</li>
+        </v-container>
         </v-container>
         <v-container>
           Copyright &copy; {{ (new Date()).getFullYear() }} Alexandre ZANNI
@@ -356,14 +375,28 @@
 
   // https://vuetifyjs.com/en/components/combobox/
   // https://pictogrammers.github.io/@mdi/font/1.1.34/
+
   export default defineComponent({
     data () {
       return {
-        select: [],
-        items: [
+        checkboxIsElectronicDocument: false,
+        checkboxInsidePublication: false,
+        inputBookOrArticle: 'Livre',
+        inputShortCitationMain: 'Oui',
+        inputEdOrDirOrCollMain: 'éd.',
+        inputEdOrDirOrCollInside: 'éd.',
+        selectIsBook: true,
+        selectAuthorsMain: [],
+        selectAuthorsInside: [],
+        itemsAuthorsMain: [
           'Dupont, S. M.',
           'Marignan, I.',
         ],
+        itemsAuthorsInside: [
+          'Dupont, S. M.',
+          'Marignan, I.',
+        ],
+        isInputPPNUnfilled: true,
       }
     },
     mounted () {
@@ -375,6 +408,25 @@
         copyText.setSelectionRange(0, 99999) // For mobile devices
         document.execCommand('copy')
         alert('Copied the text: ' + copyText.value)
+      },
+      hideIsContributionCheckBox (event) {
+        if (event === 'Livre') {
+          this.inputShortCitationMain = 'Oui'
+          this.selectIsBook = true
+        } else {
+          this.checkboxInsidePublication = false
+          this.inputShortCitationMain = 'Non'
+          this.selectIsBook = false
+        }
+      },
+      disableBtnConfirmPpn (event) {
+        if (event !== undefined && event.length > 0) {
+          this.isInputPPNUnfilled = false
+          console.log(document.getElementById('buttonConfirmPPN').disabled = '')
+          console.log(document.getElementById('buttonConfirmPPN'))
+        } else {
+          this.isInputPPNUnfilled = true
+        }
       },
     },
   })
